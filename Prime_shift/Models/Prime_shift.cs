1fc986088985af0d27fe080da85b12bb1a9a88ac
Patrick Osten <Patrickosten040@gmail.com>
2020-12-09 15:36:31 -0800

    add prime sorting functions
    Co-authored-by: Chelsea Becker <cschweig2@gmail.com>

--- Prime_shift/Models/Prime_shift.cs
+++ Prime_shift/Models/Prime_shift.cs
@@ -1,15 +1,37 @@
 using System.Collections.Generic;
+using System;
 
 namespace Prime_shift
 {
   public class Prime
   {
-    public int function (int userInput)
+    public List<int> MakeList (int userInput)
+    {
+      List<int> list = new List<int>();
+      for(int i =2; i<=userInput; i++)
       {
-        List<int> list = new List<int>();
-        for(int i =2; i<=userInput; i++)
-        {
-          list.add(i);
-        }
+        list.Add(i);
       }
+      return list;
+    }
+    public List<int> SortList (List<int> list, int prime)
+    {
+      while(prime <= userInput)
+      {
+        foreach (int number in list)
+        {
+          if (number % prime == 0)
+          {
+            list.Remove(number);
+          }
+          else
+          {
+            continue;
+          }
+        }
+        prime++;
+      }
+      return list;
+    }
+  }
 }
--- Prime_shift/Program.cs
+++ Prime_shift/Program.cs
@@ -1,14 +1,20 @@
 using System.Collections.Generic;
 using System;
 
-
 namespace Prime_shift
 {
   public class Program
   {
     public static void Main()
     {
+      Prime method = new Prime();
+      int userInput =21;
+      List<int> arr = method.MakeList(userInput);
+      int prime = 2;
       
+      foreach (int number in arr){
+        Console.WriteLine(number);
+      }
     }
   }
 }
